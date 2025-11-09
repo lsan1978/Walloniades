89906b506de7decaf6e4fb72390fa1330b0411b7
lsan1978 <laurent.sancinito@gmail.com>
2025-11-09 22:50:01 +0100

    Walloniades_20251109_gitignore_added

--- build/.qt/deploy_Walloniades_1c0fc81bbe.cmake
+++
@@ -1,8 +0,0 @@
-include(C:/C++/Walloniades/build/.qt/QtDeploySupport.cmake)
-include("${CMAKE_CURRENT_LIST_DIR}/Walloniades-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/C++/Walloniades/build/Walloniades.exe
-    GENERATE_QT_CONF
-)
--- build/CMakeFiles/Walloniades_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for Walloniades_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
